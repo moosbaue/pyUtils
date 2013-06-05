89834783cf3ea262901e15f5fc93023d348b2994
moosbaue <josef@moosbauer.net>
2013-06-05 19:09:55 +0200

    some DRY'ing

--- py_openssl_crypto.py
+++ py_openssl_crypto.py
@@ -1,4 +1,4 @@
-"""python frontend to open ssl en- decryption 
+"""python frontend to open ssl en- decryption by josef@moosbauer.net
     input parameters
         inp        string to en- decrypt
         alg        algorithm to be used, defaults to des3
@@ -10,27 +10,29 @@
         on error
             ret[0]    ''
             ret[1]    error message
+            
+    and just as reminder for myself
+        to use moduls from pyutils append to Pythonpath
+        import sys
+        sys.path.append('path to pyutils')
 """
 
 import subprocess
 
-err_msg = "this sould not happen - arghh, "
+err_msg = "this should not happen - arghh, "
 
 def doEncrypt(inp, alg='des3', passFile="~/.ssh/id_dsa"):
-    args =['openssl', 'enc', '-'+alg, '-pass', 'pass:`sed -n 2,2p '+passFile+'`', '-a']
-    try:
-        p1 = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        ret = p1.communicate(inp)
-    except:
-        return ('', err_msg + sys.exc_info()[0])
-    else:    
-        p1.stdin.close()
-        p1.stderr.close()
-        p1.stdout.close()
-        return ret
+    return _doCryptOps(inp, alg, passFile, dec=0)
 
 def doDecrypt(inp, alg='des3', passFile="~/.ssh/id_dsa"):
-    args =['openssl', 'enc', '-'+alg, '-pass', 'pass:`sed -n 2,2p '+passFile+'`', '-a', '-d']
+    return _doCryptOps(inp, alg, passFile, dec=1)
+
+def _doCryptOps(inp, alg, passFile, dec=0):
+    if dec == 0:
+        args =['openssl', 'enc', '-'+alg, '-pass', 'pass:`sed -n 2,2p '+passFile+'`', '-a']
+    else:
+        args =['openssl', 'enc', '-'+alg, '-pass', 'pass:`sed -n 2,2p '+passFile+'`', '-a', '-d']
+    
     try:
         p1 = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         ret = p1.communicate(inp)
