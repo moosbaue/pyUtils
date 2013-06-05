b42433ce268e302af74e0b91045dacda1e08f073
moosbaue <josef@moosbauer.net>
2013-06-05 19:58:13 +0200

    make line from passfile configurable

--- py_openssl_crypto.py
+++ py_openssl_crypto.py
@@ -1,8 +1,9 @@
 """python frontend to open ssl en- decryption by josef@moosbauer.net
     input parameters
         inp        string to en- decrypt
-        alg        algorithm to be used, defaults to des3
-        passFile   salt file, 2nd line used, defaults to ~/.ssh/id_dsa
+        alg        string algorithm to be used, defaults to des3
+        passFile   string salt file, defaults to ~/.ssh/id_dsa
+        line       int line to be used from passfile, defaults to 2
     output parameter
         on ok
             ret[0]    en- decrypted string
@@ -21,17 +22,17 @@ import subprocess
 
 err_msg = "this should not happen - arghh, "
 
-def doEncrypt(inp, alg='des3', passFile="~/.ssh/id_dsa"):
+def doEncrypt(inp, alg='des3', passFile="~/.ssh/id_dsa",line=2):
     return _doCryptOps(inp, alg, passFile, dec=0)
 
-def doDecrypt(inp, alg='des3', passFile="~/.ssh/id_dsa"):
+def doDecrypt(inp, alg='des3', passFile="~/.ssh/id_dsa",line=2):
     return _doCryptOps(inp, alg, passFile, dec=1)
 
-def _doCryptOps(inp, alg, passFile, dec=0):
+def _doCryptOps(inp, alg, passFile, line=2, dec=0):
     if dec == 0:
-        args =['openssl', 'enc', '-'+alg, '-pass', 'pass:`sed -n 2,2p '+passFile+'`', '-a']
+        args =['openssl', 'enc', '-'+alg, '-pass', 'pass:`sed -n '+str(line)+','+str(line)+'p '+passFile+'`', '-a']
     else:
-        args =['openssl', 'enc', '-'+alg, '-pass', 'pass:`sed -n 2,2p '+passFile+'`', '-a', '-d']
+        args =['openssl', 'enc', '-'+alg, '-pass', 'pass:`sed -n '+str(line)+','+str(line)+'p '+passFile+'`', '-a', '-d']
     
     try:
         p1 = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
